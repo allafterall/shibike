95f4c382230fd3b3518fbd4e23604c3591e6c77c
allafterall <allafterall@gmail.com>
2025-05-08 15:17:01 +0100

    Cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://annashmitko.com/',
+  site: 'https://shibike.co.uk/',
   integrations: [tailwind()]
 });
